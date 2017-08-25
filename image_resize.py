a887abaf39efb99303078d68010048bde51f8902
mcproger <artem7lomonosov@gmail.com>
2017-08-25 15:21:59 +0300

    Add get_argparser function

--- image_resize.py
+++ image_resize.py
@@ -1,3 +1,21 @@
+from PIL import Image
+import argparse
+
+
+def get_argparser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filepath', type=str,
+                        help='Path to directory with image')
+    parser.add_argument('--output', type=str,
+                        help='Path for output image')
+    parser.add_argument('--scale', type=int,
+                        help='Scale for increase or decrease image')
+    parser.add_argument('--width', type=int,
+                        help='Width for increase or decrease image')
+    parser.add_argument('--height', type=int,
+                        help='Height for increase or decrease image')
+    args = parser.parse_args()
+    return args
 
 
 def resize_image(path_to_original, path_to_result):
@@ -5,4 +23,6 @@ def resize_image(path_to_original, path_to_result):
 
 
 if __name__ == '__main__':
-    pass
+    args = get_argparser()
+    print(args.scale, args.width)
+	
