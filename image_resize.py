f0f165ca0f3699805555ffbf28fdf5834b855568
mcproger <artem7lomonosov@gmail.com>
2017-08-28 22:40:39 +0300

    Update with using resize method

--- image_resize.py
+++ image_resize.py
@@ -11,12 +11,26 @@ def get_argparser():
                         help='Path for output image')
     parser.add_argument('--scale', type=float,
                         help='Scale for increase or decrease image')
-    parser.add_argument('--width', type=int,
+    parser.add_argument('--width', type=float,
                         help='Width for increase or decrease image')
-    parser.add_argument('--height', type=int,
+    parser.add_argument('--height', type=float,
                         help='Height for increase or decrease image')
     args = parser.parse_args()
     return args
+
+
+def get_path_to_result_image(args, resize_width, resize_height):
+    if args.output:
+        path_to_result_image = args.output
+        return path_to_result_image
+    filename, extension = os.path.splitext(args.filepath)
+    path_to_result_image = '{filename}_{resize_width}x{resize_height}{extension}'.format(
+        filename=filename,
+        resize_width=resize_width,
+        resize_height=resize_height,
+        extension=extension
+    )
+    return path_to_result_image
 
 
 def get_original_image_info(path_to_original):
@@ -25,46 +39,40 @@ def get_original_image_info(path_to_original):
     return original_image, original_width, original_height
 
 
-def get_path_to_result_image(args, width, height):
-    if args.output:
-        path_to_result_image = args.output
-        return path_to_result_image
-    filename, extension = os.path.splitext(args.filepath)
-    path_to_result_image = '{filename}_{width}x{height}{extension}'.format(
-        filename=filename,
-        width=width,
-        height=height,
-        extension=extension
-    )
-    return path_to_result_image
-
-
-def get_image_resize_area(args, image_proportion, original_width, original_height):
+def get_resize_info(args, original_width, original_height, image_proportion):
     if args.scale and not args.width and not args.height:
-        resize_area = (0, 0, original_width * args.scale,
-                       original_height * args.scale)
-    elif args.width and not args.height:
-        resize_area = (0, 0, args.width, args.width / image_proportion)
-    elif args.height and not args.width:
-        resize_area = (0, 0, args.height * image_proportion, args.height)
+        resize_width = args.scale * original_width
+        resize_height = args.scale * original_height
+        return resize_width, resize_height
+    elif args.width and not args.scale and not args.height:
+        resize_width = args.width
+        resize_height = args.width / image_proportion
+        return resize_width, resize_height
+    elif args.height and not args.scale and not args.width:
+        resize_width = args.height * image_proportion
+        resize_height = args.height
+        return resize_width, resize_height
+    elif args.height and args.width and not args.scale:
+        return args.height, args.width
     else:
-        resize_area = (0, 0, args.width, args.height)
-    return resize_area
+        return None
 
 
-def get_resize_image(original_image, path_to_result_image, resize_area):
-    resize_image = original_image.crop(resize_area)
+def get_resize_image(original_image, path_to_result_image, resize_width, resize_height):
+    resize_image = original_image.resize(
+        (resize_width, resize_height), Image.ANTIALIAS)
     resize_image.save(path_to_result_image)
     return None
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = get_argparser()
     original_image, original_width, original_height = get_original_image_info(
         args.filepath)
     image_proportion = original_width / original_height
+    resize_width, resize_height = get_resize_info(
+        args, original_width, original_height, image_proportion)
     path_to_result_image = get_path_to_result_image(
-        args, original_width, original_height)
-    resize_area = get_image_resize_area(
-        args, image_proportion, original_width, original_height)
-    get_resize_image(original_image, path_to_result_image, resize_area)
+        args, round(resize_width), round(resize_height))
+    resize_image = get_resize_image(
+        original_image, path_to_result_image, round(resize_width), round(resize_height))
